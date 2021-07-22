1b7bf9c8209ee3ddaca7ba27bb755245ad9b16b1
dehaotu <dehaotu@gmail.com>
2021-07-21 19:24:42 -0700

    minor fix to pause

--- Assets/_Scripts/Instruction_pause.cs
+++ Assets/_Scripts/Instruction_pause.cs
@@ -9,17 +9,30 @@ public class Instruction_pause : MonoBehaviour
     public static bool GamePaused = false;
     public GameObject pauseMenuUI;
     // Update is called once per frame
+    private AudioSource[] audioSources;
+
+    private void Start()
+    {
+        audioSources = GameObject.FindObjectsOfType<AudioSource>();
+    }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GamePaused = false;
-
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            audioSources[i].Play();
+        }
     }
 
-    void Pause()
+    public void Pause()
     {
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            audioSources[i].Pause();
+        }
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GamePaused = true;
