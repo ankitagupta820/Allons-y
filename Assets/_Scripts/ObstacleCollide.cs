0745d35b9d7f2e23436680ac8fe4b31226749ddf
Ankita Gupta <ankitag@usc.edu>
2021-07-21 20:06:28 -0700

    Obstacles

---
+++ Assets/_Scripts/ObstacleCollide.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollide : MonoBehaviour
+{
+   
+    public string collectableLabel; // Label for letter type to be popped out by the asteroid.
+    private ScoreManager scoreManager; 
+
+    void Start()
+    {
+        scoreManager = ScoreManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        GameObject playerGO = other.gameObject;
+        if (playerGO.tag == "Player")
+        {
+            int collectedObjects = ScoreManager.getCollectibleCollectedNumber(collectableLabel);
+            Debug.Log(collectedObjects);
+            scoreManager.popOutCollectibles(collectableLabel, collectedObjects);
+        }
+    }
+}
